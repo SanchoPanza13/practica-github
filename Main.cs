fdc49c7871d39fbba74df226d7ae9f0757249dd0
SanchoPanza13 <wayan.el.sanchez13@gmail.com>
2025-01-21 19:50:59 +0100

    Create Main.cs
    He creado un main.cs con un versiculo de corán aleatorio y gamberradas de turno de Angelo

---
+++ Main.cs
@@ -0,0 +1,7 @@
+[27:83] Mas cuando se dicte sentencia
+contra ellos, haremos brotar para ellos una
+criatura de la tierra, que los herirá porque las
+gentes no creyeron en Nuestros Signos.
+locked in
+alien
+lingangu lingangu SALAMELECOMELECOMSALAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
